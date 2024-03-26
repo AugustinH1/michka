52dc5b44900bb081b4f01925935eef4f6fa05f18
AugustinH1 <augustin.husson2@gmail.com>
2024-03-26 10:55:57 +0100

    fix: javafx lib

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,12 @@
 module michka {
     requires javafx.controls;
     requires jakarta.persistence;
+    requires java.sql;
+    requires org.hibernate.orm.core;
+
+    opens Entity to org.hibernate.orm.core;
 
     exports Graphics;
+    exports main;
+
 }
